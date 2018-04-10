aba82e663533bb3e0df2b8b1b67e86ab07989b7a
axayhegde <akshayheg@gmail.com>
2018-04-10 18:28:30 +0530

    matrix programs

---
+++ matrixAddition.js
@@ -0,0 +1,12 @@
+var arr = [1, 2, 3, 4]
+var arr1 = [2, 3, 4, 5];
+var ans = [];
+
+if (arr.length = arr1.length) {
+    for (var i = 0; i < arr.length; i++) {
+
+        ans[i] = arr[i] + arr1[i];
+
+    }
+}
+console.log(ans);
