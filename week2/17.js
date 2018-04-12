3df0e78dc0ed3c6240a7520cbb3f0ae21910a781
axayhegde <akshayheg@gmail.com>
2018-04-12 17:05:39 +0530

    17 programs done

--- week2/17.js
+++ week2/17.js
@@ -4,26 +4,8 @@
 
 function iterativeBinaryRepNumber(number) {
 
-    //Checking if numbers 0th bit is 1 or 0
+   console.log((number >>> 0).toString(2));
 
-    var bit;
-    var arr = [];
-    var binaryRep = "";
-
-    for (var i = 0; i <= 8; i++) {
-        bit = Math.pow(2,i);
-        console.log(bit);
-        if (number & bit == 1) {
-            arr[i] = 1;
-            binaryRep =  '1' + binaryRep;
-        }else{
-            arr[i] = 0;
-            binaryRep =  '0' + binaryRep ;
-        }
-    }
-
-    console.log('Binary Rep -----> '+ binaryRep);
-    console.log('Array ----> ' + arr);
 }
 
-iterativeBinaryRepNumber(5);
+iterativeBinaryRepNumber(10);
