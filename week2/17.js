cee882dfe79181bb8eb3ed6d76d3f53657ea4d95
axayhegde <akshayheg@gmail.com>
2018-04-12 17:18:45 +0530

    actually read the question properly

--- week2/17.js
+++ week2/17.js
@@ -4,7 +4,20 @@
 
 function iterativeBinaryRepNumber(number) {
 
+    //Answer
    console.log((number >>> 0).toString(2));
+
+    var totalBits = Math.floor(Math.log2(number) + 1);
+
+    var binaryNumber = "";
+
+    for(i = 0; i < totalBits; i++){
+        var ithBit = ((number & (Math.pow(2,i))) === 0)? 0:1;
+        binaryNumber = ithBit + binaryNumber;
+    }
+
+    console.log('Binary Number ---> ' + binaryNumber);
+
 
 }
 
