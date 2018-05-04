fc523e8238f8310d29f41ac0d054715a234640a1
axayhegde <akshayheg@gmail.com>
2018-05-04 13:37:19 +0530

    updated stack program

--- stackJs.js
+++ stackJs.js
@@ -25,7 +25,16 @@ function Stack(){
         items = [];
     };
     this.print = function(){
-        console.log(items.toString());
+        console.log(items);
+    }
+    this.search = function(value){
+        console.log('Searching for value --> ' + value);
+        if(items.includes(value)){
+            console.log('Value Present in stack');
+        }else{
+            console.log('Value ' + value +' not found in stack');
+        }
+
     }
 }
 
@@ -33,6 +42,11 @@ let stack = new Stack();
 
 stack.push(1);
 stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+
+stack.search(6);
 
 console.log(stack.print());
 
