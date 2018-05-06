2365dbfb1217c08f52b2b722cd3c7f269b431780
axayhegde <akshayheg@gmail.com>
2018-05-06 18:12:44 +0530

    added palindrome

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+//Check if a string is a palindrome
+
+
+function palindrome(str){
+
+    var len = str.length;
+    for ( var i = 0; i < Math.floor(len/2); i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+
+}
+
+var myString = "NITIN";
+
+console.log(palindrome(myString));
