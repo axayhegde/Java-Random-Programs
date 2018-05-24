72ca2ab8b3dfb669641e18d333fa033c6acc735e
axayhegde <akshayheg@gmail.com>
2018-05-24 09:31:33 +0530

    added regex to remove numbers and operators

--- arrayHelper/paranthesisMatchingReduce.js
+++ arrayHelper/paranthesisMatchingReduce.js
@@ -1,13 +1,16 @@
 //Balancing the parathesis string using reduce
 
-
 var str = "(())";
 var str1 = "(()";
 var str2 = "(()())";
+var str3 = "(1 + (2) + (3))";
 
-var strArr = str2.split("");
+var desired = str3.replace(/[0-9 +\-*\/]/gm,'');
+var strArr = desired.split("");
 
-const isParanBalance = (strArr) =>  strArr.reduce((prev,char) => {
+console.log(str3);
+
+const isParamBalance = (strArr) =>  strArr.reduce((prev,char) => {
 
     if(prev < 0){
         return prev;
@@ -21,7 +24,7 @@ const isParanBalance = (strArr) =>  strArr.reduce((prev,char) => {
     return prev;
 
 }, 0)
-if(isParanBalance(strArr) === 0){
+if(isParamBalance(strArr) === 0){
     console.log("The Parenthesis are balanced!");
 }else{
     console.log("The Parenthesis are NOT balanced!");
